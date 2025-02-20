44a386487b83a6bac943ed67ba3ac049859c01e8
PraveenNagaraj77 <praveennagaraj76@gmail.com>
2025-02-20 15:27:35 +0530

    New

--- Basics/src/Datatypes.java
+++ Basics/src/Datatypes.java
@@ -18,16 +18,6 @@ public class Datatypes {
         Date today = new Date();
         System.out.println(today);
 
-        Point p1=new Point(5,7);
-        Point p2= p1;
-        System.out.println(p2);
-
-        p1.x=7;
-
-
-        System.out.println(p1);
-        System.out.println(p2);
-
 
 
     }
